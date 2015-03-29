57c321cb0ce26dcc436d713763989d1a25327bbb
FxNion <Fx@BigMac.local>
2015-03-29 17:21:35 +0200

    Handling inlayout login box

--- views/default/custom_index_widgets/index_2rbms.php
+++ views/default/custom_index_widgets/index_2rbms.php
@@ -4,13 +4,18 @@
 		$area3widgets = $vars['area3'];
 		$layoutmode   = $vars['layoutmode']; //edit, index
 		
+		$login_style = get_plugin_setting("login_style", "custom_index_widgets");
+		if (!isset($login_style)) {
+			$login_style = 'inlayout';
+		}
+		
 ?>
 
     <table cellspacing="10" cellpadding="10" width="100%" class="<?php echo elgg_echo($layoutmode); ?>">
       <tr>
         <td colspan="2" align="left" valign="top" class="<?php echo elgg_echo($layoutmode); ?>">
           <?php if ($layoutmode == 'edit_mode') {?> 
-              <h2 class=big_<?php echo elgg_echo($layoutmode); ?>_box"><?php echo elgg_echo("custom_index_widgets:area1"); ?></h2>
+              <h2 class="big_<?php echo elgg_echo($layoutmode); ?>_box"><?php echo elgg_echo("custom_index_widgets:area1"); ?></h2>
           <?php }?>
               <div id="leftcolumn_widgets" class="big_<?php echo elgg_echo($layoutmode); ?>_box">
               <?php if (is_array($area1widgets) && sizeof($area1widgets) > 0)
@@ -44,7 +49,7 @@
           <?php }?>
               <div id="rightcolumn_widgets" class="small_<?php echo elgg_echo($layoutmode); ?>_box">
               	<?php
-            	 	if (!isloggedin())
+            	 	if (!isloggedin() && $login_style == 'inlayout')
             	 		echo elgg_view("account/forms/login"); 
             	?>
               <?php if (is_array($area3widgets) && sizeof($area3widgets) > 0)
